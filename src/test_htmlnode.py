49107c042a7b5d635b48dc976f096bdb5ba59be9
LiamWalshTech <admin@liamwalsh.tech>
2025-04-23 22:19:39 +0100

    Shamelessly stolen from the solution files but after I already solved mine

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -4,12 +4,51 @@ from htmlnode import HTMLNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    def test_eq(self):
-        node = HTMLNode("h1", "Google", [], {
-            "href": "https://www.google.com",
-            "target": "_blank",
-        })
-        self.assertEqual(node.props_to_html(), 'href="https://www.google.com" target="_blank"')
+    def test_to_html_props(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
+        )
+
+    def test_values(self):
+        node = HTMLNode(
+            "div",
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.tag,
+            "div",
+        )
+        self.assertEqual(
+            node.value,
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.children,
+            None,
+        )
+        self.assertEqual(
+            node.props,
+            None,
+        )
+
+    def test_repr(self):
+        node = HTMLNode(
+            "p",
+            "What a strange world",
+            None,
+            {"class": "primary"},
+        )
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
+        )
 
 if __name__ == "__main__":
     unittest.main()
