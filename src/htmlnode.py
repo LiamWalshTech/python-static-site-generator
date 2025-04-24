89c6c8c5be06e304dda0cce9be91dad51114760b
LiamWalshTech <admin@liamwalsh.tech>
2025-04-24 20:37:28 +0100

    Passed and running but using solution versions so that later tests all cacade well

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,18 +1,20 @@
 class HTMLNode:
-    def __init__(self, tag = None, value = None, children = None, props = None):
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
 
     def to_html(self):
-        raise NotImplementedError()
+        raise NotImplementedError("to_html method not implemented")
 
     def props_to_html(self):
-        props_list = []
-        for key, value in self.props.items():
-            props_list.append(f'{key}="{value}"')
-        return ' '.join(props_list)
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
--- src/leafnode.py
+++ src/leafnode.py
@@ -1,14 +1,15 @@
 from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag, value):
-        super().__init__(tag, value)
-    
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
     def to_html(self):
-        if self.value == None:
-            raise ValueError()
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
-        if self.tag == None:
-            return value
-
-        return f'<{self.tag}>{self.value}</{self.tag}>'
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
