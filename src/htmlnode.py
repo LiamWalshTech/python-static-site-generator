d4a1629847027de572c82a2fded030ea247917a5
LiamWalshTech <admin@liamwalsh.tech>
2025-04-24 20:09:46 +0100

    Finishing tests for C2 L3

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -15,4 +15,4 @@ class HTMLNode:
         return ' '.join(props_list)
 
     def __repr__(self):
-        return f"HTMLNode({self.tag} {self.value} {self.children} {self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
