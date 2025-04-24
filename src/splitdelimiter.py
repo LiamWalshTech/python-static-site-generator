0774fe76d50c7384c74d7e18ead6822faeabb9ce
LiamWalshTech <admin@liamwalsh.tech>
2025-04-24 21:55:35 +0100

    Taking the more advanced function for moving forward

--- src/splitdelimiter.py
+++ src/splitdelimiter.py
@@ -1,12 +1,21 @@
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    nodes_array = []
-
+    new_nodes = []
     for old_node in old_nodes:
-        text_split = old_node.text.split(delimiter)
-        nodes_array.append(TextNode(text_split[0], TextType.NORMAL))
-        nodes_array.append(TextNode(text_split[1], TextType.CODE))
-        nodes_array.append(TextNode(text_split[2], TextType.NORMAL))
-
-    return nodes_array
+        if old_node.text_type != TextType.NORMAL:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.NORMAL))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
